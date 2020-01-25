d82b833c7115268ec8dc14dc42020d239332f6a9
aman shah <shah.ama@husky.neu.edu>
2020-01-24 23:18:48 -0500

    lab one hardware utilization

--- apps/tests/labs/Module01Test.py
+++ apps/tests/labs/Module01Test.py
@@ -1,8 +1,12 @@
 import unittest
+#import labs.module01.SystemCPUUtilTask
+#import labs.module01.SystemMemoryUtilTask 
+from labs.module01.SystemMemoryUtilTask import SystemMemoryUtilTask
+from labs.module01.SystemCPUUtilTask import SystemCPUUtilTask
 
 
 """
-Test class for all requisite Module01 functionality.
+Test class for all requisite Module02 functionality.
 
 Instructions:
 1) Rename 'testSomething()' method such that 'Something' is specific to your needs; add others as needed, beginning each method with 'test...()'.
@@ -20,20 +24,57 @@ class Module01Test(unittest.TestCase):
 	information (if needed), initialize class-scoped variables, create class-scoped
 	instances of complex objects, initialize any requisite connections, etc.
 	"""
-	def setUp(self):
-		pass
-
+	
 	"""
 	Use this to tear down any allocated resources after your tests are complete. This
 	is where you may want to release connections, zero out any long-term data, etc.
 	"""
-	def tearDown(self):
-		pass
+	def test_checkfloat(self):
+		
+		
+		"""
+		we will use this method to check if the return type from psutils
+		are float or some other value
+		
+		we will instantiate classes 
+		the unit test should assert true is the return type  of method are 
+		of float type
+		else the test should fail 
+		
+		
+		"""
+		memvar= SystemMemoryUtilTask()
+		cpuvar = SystemCPUUtilTask()
+		
+		
+		
+		self.assertTrue(isinstance(memvar.calculateMemory(), float)," its a float")
+		self.assertTrue(isinstance(cpuvar.calculatecpuUtil(),float)," it is a float")
+		#pass
 	
 	"""
 	Place your comments describing the test here.
 	"""
-	def testSomething(self):
+	def test_range(self):				
+		
+		
+		"""
+		we are testing if the cpu util and mem util is in range of 0 to 100
+		since it is inpercent values it will be below 100
+		we will initialize classes which we want to test 
+		test cases should return true if conditions are met properly else should
+		return test fail
+		"""
+		
+		memvar= SystemMemoryUtilTask()
+		cpuvar = SystemCPUUtilTask()
+		
+		
+		self.assertTrue( 0.0 < memvar.calculateMemory()< 100.0 , " it is in range")
+		self.assertTrue( 0.0 < cpuvar.calculatecpuUtil() < 100.0 , " it is in range")
+		
+		
+		#self.assertTrue(isinstance(cpuvar.calculatecpuUtil(), float), "it is a float")
 		pass
 
 if __name__ == "__main__":
