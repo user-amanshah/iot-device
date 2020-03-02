c9fec40f1a428a4e97f2ecc34441f1f1ad9bf6ad
aman <shah.ama@husky.neu.edu>
2020-03-02 04:20:39 +0000

    DeviceHandlerApp.py edited online with Bitbucket

--- apps/labs/module06/DeviceHandlerApp.py
+++ apps/labs/module06/DeviceHandlerApp.py
@@ -6,7 +6,7 @@ Created on Feb 29, 2020
 
 
 from labs.module06.MultiSensorAdaptor import MultiSensorAdaptor 
-
+#main function script 
 if __name__ == '__main__':
     device_manager = MultiSensorAdaptor()
     device_manager.adaptor()
